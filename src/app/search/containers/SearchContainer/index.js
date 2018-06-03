f5985f4a1839cb7fb41fa6f8408db9fae956016a
Juan Pablo Duque Ramirez <juan.duque@globant.com>
2018-06-02 20:08:18 -0500

    Implements grid system on SearchContainer

--- src/app/search/containers/SearchContainer/index.js
+++ src/app/search/containers/SearchContainer/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+
 import { connect } from 'react-redux'
+import { Container, Row, Col } from 'react-grid-system';
 
 import { filtersOperations } from '../../../state/modules/filters';
 import { hotelsOperations } from '../../../state/modules/hotels';
 
+import {
+    HeaderComponent,
+    LogoComponent
+ } from "../../../common/components";
+
 import SearchComponent from "../../components/SearchComponent";
+
+import HotelListContainer from "../HotelListContainer";
 
 const mapStateToProps = ({
     filtersState: { 
@@ -29,9 +38,10 @@ const mapDispatchToProps = dispatch => ({
     handleSearchHotels: (hotelsName, stars) => (
         dispatch(hotelsOperations.searchHotels(hotelsName, stars))
     ),
-    handleSelectAll: () => (
-        dispatch(filtersOperations.selectAllStars())
-    )
+    handleSelectAll: (hotelsName, stars) => {
+        dispatch(filtersOperations.selectAllStars());
+        dispatch(hotelsOperations.searchHotels(hotelsName, stars, null, true));
+    }
 });
 
 const SearchContainer = connect(
@@ -46,14 +56,24 @@ const SearchContainer = connect(
     hotelsName
 }) => (
     <div>
-        <SearchComponent 
-            stars={stars} 
-            hotelsName={hotelsName}
-            onToggleStar={(star) => handleToggleStar(hotelsName, stars, star)}
-            onSelectAll={handleSelectAll}
-            onChangeHotelsName={handleHotelsNameChange}
-            onSearchHotels={handleSearchHotels}
-        />
+        <HeaderComponent>
+            <LogoComponent/>
+        </HeaderComponent>
+        <Container fluid>
+            <Row>
+                <SearchComponent 
+                    stars={stars} 
+                    hotelsName={hotelsName}
+                    onToggleStar={(star) => handleToggleStar(hotelsName, stars, star)}
+                    onSelectAll={() => handleSelectAll(hotelsName, stars)}
+                    onChangeHotelsName={handleHotelsNameChange}
+                    onSearchHotels={handleSearchHotels}
+                />
+                <Col>
+                    <HotelListContainer/>           
+                </Col>
+            </Row>
+        </Container>
     </div>
 ));
 
