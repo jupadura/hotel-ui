4882f49d429fed31983c7e4fc25d9cedaa88afb5
Juan Pablo Duque Ramirez <juan.duque@globant.com>
2018-06-02 20:31:34 -0500

    Improves api call to allow  searching by all stars

--- src/app/state/modules/hotels/operations.js
+++ src/app/state/modules/hotels/operations.js
@@ -1,15 +1,20 @@
 import fetch from "cross-fetch";
 import actions from "./actions";
 
-const searchHotels = (name, stars, star) => dispatch => {
-    const method = stars.has(star) ? 'delete' : 'add';
-    const newStart = star ? stars[method](star) : stars;
+const searchHotels = (name, stars, star, allStars) => dispatch => {
+    let newStars;
+    if (allStars) {
+        newStars = stars.size === 5 ? [] : [1, 2, 3, 4, 5];
+    } else {
+        const method = stars.has(star) ? 'delete' : 'add';
+        newStars = star ? stars[method](star) : stars;
+    }
 
-    return fetch(`http://localhost:3001/api/hotels?name=${name}&stars=${newStart.join(',')}`)
+    return fetch(`http://localhost:3001/api/hotels?name=${name}&stars=${newStars.join(',')}`)
         .then(
             response => response.json(),
         )
-        .then(hotels => dispatch(actions.getHotelsSuccess(hotels, name, newStart)));
+        .then(hotels => dispatch(actions.getHotelsSuccess(hotels, name, newStars)));
 };
 
 export default {
